3ed45603efbba386b6010eec9591eab0560b1a79
Alex <alexperrier.pro@gmail.com>
2023-01-17 16:29:06 +0100

    add new reducer "user slice" to redux store

--- frontend/src/app/store.js
+++ frontend/src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import loginReducer from "../features/loginSlice";
+import userReducer from "../features/userSlice";
 
 const store = configureStore({
   reducer: {
     login: loginReducer,
+    user: userReducer,
   },
 });
 
